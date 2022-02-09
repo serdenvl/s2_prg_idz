5ee13c42938fda09a441187cddf6c76a7ae47c29
serdenvl <serdenvl@gmail.com>
2022-02-09 22:16:16 +0700

    +idz#7

--- src/task7.h
+++ src/task7.h
@@ -2,6 +2,31 @@
 #include <stdlib.h>
 #include "utils.h"
 
+struct Date
+{
+    int day;
+    int month;
+    int year;
+};
+
+int is_correct_date(struct Date date)
+{
+    if(date.year <= 0 || date.day <= 0 || date.month <= 0)
+        return 0;
+    
+    if(12 < date.month)
+        return 0;
+    
+    if( 30+((date.month < 8) == date.month%2) < date.day )
+        return 0;
+    
+    if(date.month == 2 && (28 + (date.year%4==0) < date.day))
+        return 0;
+    
+    return 1;
+}
+
+
 /*
     Написать и протестировать функцию, проверяющую правильность даты (т.е. чтобы не было 31 июня и т.п.). 
     Для описания даты использовать структуру. 
@@ -18,7 +43,11 @@ int task7()
     localize();
     welcome(task_name, welcome_message);
 
+    red_print("Введите дату: ");
 
+    struct Date date = {scan_int(""), scan_int(""), scan_int("")};
+
+    red_print((is_correct_date(date)) ? "Дата корректна" : "Дата некорректна");
 
     getchar();
 }
