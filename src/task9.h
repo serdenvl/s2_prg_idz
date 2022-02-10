35677b0dfc5c085043e4f6d4835d9880cfe41e53
serdenvl <serdenvl@gmail.com>
2022-02-10 13:06:34 +0700

    fix

--- src/task9.h
+++ src/task9.h
@@ -2,35 +2,36 @@
 #include <stdlib.h>
 #include "utils.h"
 
-int* lens_of_string(FILE* file)
+int *lens_of_string(FILE *file)
 {
-    int* array = new_array(1);
+    int *array = new_array(1);
     array[0] = 1;
 
     int count = 0;
     char buff;
-    while(buff != -1)
+
+    do
     {
         buff = fgetc(file);
-        if(buff == '\n' || buff == -1)
+        if (buff == '\n' || buff == -1)
         {
             array[0] += 1;
             array = resize_array(array, array[0]);
 
-            array[array[0]-1] = count;
+            array[array[0] - 1] = count;
             count = 0;
             continue;
         }
-        
+
         ++count;
-    }
+    } while (buff != -1);
 
     return array;
 }
 
 /*
-    Прочитать текстовый файл. 
-    Для каждой строки определить и вывести на экран её номер и длину. 
+    Прочитать текстовый файл.
+    Для каждой строки определить и вывести на экран её номер и длину.
 */
 
 #define task_name "idz #9"
@@ -44,15 +45,15 @@ int task9()
     localize();
     welcome(task_name, welcome_message);
 
-    FILE* file = fopen("../src/file.txt", "r");
+    FILE *file = fopen("../src/file.txt", "r");
 
-    int* lens = lens_of_string(file);
+    int *lens = lens_of_string(file);
 
     red_print("Длины строк: ");
-    for(int i = 1; i < lens[0]; ++i)
+    for (int i = 1; i < lens[0]; ++i)
         printf("\n%d: %d", i, lens[i]);
     free(lens);
-    
+
     fclose(file);
     getchar();
 }
