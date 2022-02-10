263f8fe5f3bbab4535ec8960acdc1329d7339143
serdenvl <serdenvl@gmail.com>
2022-02-10 12:19:15 +0700

    +idz#8 +idz#9

--- src/task8.h
+++ src/task8.h
@@ -2,26 +2,59 @@
 #include <stdlib.h>
 #include "utils.h"
 
+struct Baggage
+{
+    int number_items;
+    int weight;
+};
+
+struct Baggage find(struct Baggage *array, int length)
+{
+    struct Baggage finded = array[0];
+    for (int i = 1; i < length; ++i)
+    {
+        if(
+            finded.number_items < array[i].number_items ||
+            (finded.number_items == array[i].number_items && finded.weight > array[i].weight)
+        )
+            finded = array[i];
+    }
+
+    return finded;
+}
+
 /*
-    Багаж пассажира характеризуется количеством вещей и общим весом. 
-    Создать таблицу,  содержащую  информацию о багаже нескольких пассажиров.  
-    Найти багаж, число вещей в котором не меньше, чем в любом другом, а вес, не боль-ше, чем в любом другом с тем же числом вещей. 
+    Багаж пассажира характеризуется количеством вещей и общим весом.
+    Создать таблицу,  содержащую  информацию о багаже нескольких пассажиров.
+    Найти багаж, число вещей в котором не меньше, чем в любом другом, а вес, не боль-ше, чем в любом другом с тем же числом вещей.
 */
 
 #define task_name "idz #8"
 #define welcome_message "\
-*description* \n\
+Дан массив багажей (количество вещей, вес). \n\
+Вывести багаж с наибольшим количеством вещей \n\
+и наименьшим весом среди багажей с равным количеством вещей. \n\
 "
-
-// ? что значит "таблица"? два синхронных массива сойдут?
 
 int task8()
 {
     localize();
     welcome(task_name, welcome_message);
 
-    red_print("Решения ещё нет");
-    return 0;
+    int N = scan_int("Количество пассажиров: ");
+    struct Baggage array[N];
+
+    red_print("Информация о багаже: (количество_вещей вес)\n");
+    for (int i = 0; i < N; ++i)
+    {
+        array[i].number_items = scan_int("");
+        array[i].weight = scan_int("");
+    }
+
+    struct Baggage finded = find(array, N);
+
+    red_print("Найденный багаж: ");
+    printf("(%d, %d)", finded.number_items, finded.weight);
 
     getchar();
 }
--- src/task9.h
+++ src/task9.h
@@ -2,6 +2,32 @@
 #include <stdlib.h>
 #include "utils.h"
 
+int* lens_of_string(FILE* file)
+{
+    int* array = new_array(1);
+    array[0] = 1;
+
+    int count = 0;
+    char buff;
+    while(buff != -1)
+    {
+        buff = fgetc(file);
+        if(buff == '\n' || buff == -1)
+        {
+            array[0] += 1;
+            array = resize_array(array, array[0]);
+
+            array[array[0]-1] = count;
+            count = 0;
+            continue;
+        }
+        
+        ++count;
+    }
+
+    return array;
+}
+
 /*
     Прочитать текстовый файл. 
     Для каждой строки определить и вывести на экран её номер и длину. 
@@ -18,8 +44,15 @@ int task9()
     localize();
     welcome(task_name, welcome_message);
 
-    red_print("Решения ещё нет");
-    return 0;
+    FILE* file = fopen("../src/file.txt", "r");
 
+    int* lens = lens_of_string(file);
+
+    red_print("Длины строк: ");
+    for(int i = 1; i < lens[0]; ++i)
+        printf("\n%d: %d", i, lens[i]);
+    free(lens);
+    
+    fclose(file);
     getchar();
 }
